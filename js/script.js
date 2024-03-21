a9242cd768efb02036e4b57ed5dd1a21f2f3aba2
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-03-21 17:59:06 +0000

    area of triangle program

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Julianne Leblanc-Peltier All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julianne Leblanc-Peltier
+// Created on: March 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // input
+  const base = parseInt(document.getElementById("base-of-triangle").value)
+  const height = parseInt(document.getElementById("height-of-triangle").value)
+
+  // process
+  const area = 0.5 * base * height
+
+  // output
+  document.getElementById("area-of-triangle").innerHTML = area.toFixed(2) + " cm²"
+}
